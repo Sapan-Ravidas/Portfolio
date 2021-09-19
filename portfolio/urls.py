1a1967926fe4a71f460d953ae7244ffa23424d94
Sapan-Ravidas <sapanravidas@gmail.com>
2021-09-19 14:26:31 +0530

    trying static files

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static 
 from sapan import urls as app_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sapan.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
